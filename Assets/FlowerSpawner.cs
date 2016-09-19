6f1ebaa8948b97e1e1e0a765aba8c7e6e3bb5087
t-zouchi <mojamoja.000@gmail.com>
2016-09-19 14:32:34 +0900

    Add FlowerSpawner

---
+++ Assets/FlowerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerSpawner : MonoBehaviour {
+
+  public GameObject FlowerPrefab;
+
+	// Use this for initialization
+	void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetButtonDown("Fire1")) Drop();	
+	}
+
+  public void Drop()
+  {
+    GameObject flower = (GameObject)Instantiate(
+      FlowerPrefab,
+      transform.position,
+      Quaternion.identity
+      );
+    Destroy(flower, 3);
+  }
+}
