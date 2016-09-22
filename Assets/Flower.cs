a0a59b164b5bd9f3ca1871c08c2a73cee7c6407a
t-zouchi <mojamoja.000@gmail.com>
2016-09-22 11:14:11 +0900

    add SashimiAndFlower

---
+++ Assets/Flower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flower : MonoBehaviour {
+
+  public float speed;
+  public string activeTag;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void OnColligionEnter(Collision collision)
+  {
+    Debug.Log("さわさわ");
+  }
+}
---
+++ Assets/SashimiAndFlower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SashimiAndFlower : MonoBehaviour {
+  public int speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.Translate(speed * Time.deltaTime, 0, 0);
+  }
+}
