49f337ebf54fe6363b003840bebdf850da8dfd9c
t-zouchi <mojamoja.000@gmail.com>
2016-10-24 20:28:00 +0900

    Add gameover

--- Assets/FlowerSpawner.cs
+++ Assets/FlowerSpawner.cs
@@ -5,13 +5,8 @@ public class FlowerSpawner : MonoBehaviour {
 
   public GameObject FlowerPrefab;
 
-	// Use this for initialization
-	void Start () {
-    
-	}
-	
-	// Update is called once per frame
-	void Update () {
+  // Update is called once per frame
+  void Update () {
     if (Input.GetButtonDown("Fire1")) Drop();	
 	}
 
