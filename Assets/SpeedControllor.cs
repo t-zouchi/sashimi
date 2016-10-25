f76688a5b69e19381d0b05875801227865d9193f
t-zouchi <mojamoja.000@gmail.com>
2016-10-25 18:51:37 +0900

    score

--- Assets/SpeedControllor.cs
+++ Assets/SpeedControllor.cs
@@ -6,6 +6,7 @@ public class SpeedControllor : MonoBehaviour {
   public float speed = 2;
   int count = 1;
   int level = 1;
+  int limit = 6;
 
   void Start()
   {
@@ -16,12 +17,25 @@ public class SpeedControllor : MonoBehaviour {
   public void speedUp()
   {
     Debug.Log(level);
-    if (count % level == 0)
+    if (level <= limit)
     {
-      speed = speed * 1.1f;
-      count = 1;
-      level++;
+      if (count % level == 0)
+      {
+        speed = speed * 1.1f;
+        count = 1;
+        level++;
+      }
     }
+    else
+    {
+      if (count % limit == 0)
+      {
+        speed = speed * 1.1f;
+        count = 1;
+        level++;
+      }
+    }
+    
     count++;
   }
 
